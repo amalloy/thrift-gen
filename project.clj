3cd559cd0502b787e6d3c8662c6c2c8c3635f072
Alan Malloy <alan@malloys.org>
2014-10-16 16:55:09 -0700

    0.2.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject amalloy/thrift-gen "0.2.0-SNAPSHOT"
+(defproject amalloy/thrift-gen "0.2.0"
   :description "quickcheck generators for producing instances of Thrift schemas"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
